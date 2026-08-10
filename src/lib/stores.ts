fdcde72fcb4d5c5045b5b448bda584158912dd42
AlexanderBell-gh <261503542+AlexanderBell-gh@users.noreply.github.com>
2026-08-10 12:18:22 +0100

    Updated the relevant search link for Morrisons in stores.ts

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -9,7 +9,7 @@ export const STORES: Store[] = [
   { id: 'tesco', name: 'Tesco', logo: '/Tesco_Logo.svg', searchUrl: (q) => `https://www.tesco.com/groceries/en-GB/search?query=${encodeURIComponent(q)}` },
   { id: 'sainsburys', name: "Sainsbury's", logo: '/Sainsbury\'s_Logo.svg', searchUrl: (q) => `https://www.sainsburys.co.uk/gol-ui/SearchResults/${encodeURIComponent(q)}` },
   { id: 'asda', name: 'ASDA', logo: '/ASDA_Logo.svg', searchUrl: (q) => `https://groceries.asda.com/search/${encodeURIComponent(q)}` },
-  { id: 'morrisons', name: 'Morrisons', logo: '/Morrisons_Logo.svg', searchUrl: (q) => `https://www.morrisons.com/search?q=${encodeURIComponent(q)}` },
+  { id: 'morrisons', name: 'Morrisons', logo: '/Morrisons_Logo.svg', searchUrl: (q) => `https://groceries.morrisons.com/search?q=${encodeURIComponent(q)}` },
   { id: 'marksandspencer', name: 'M&S', logo: '/M&S_Logo.svg', searchUrl: (q) => `https://www.marksandspencer.com/l/search?q=${encodeURIComponent(q)}` },
   { id: 'aldi', name: 'Aldi', logo: '/Aldi_Logo.svg', searchUrl: (q) => `https://www.aldi.co.uk/search?q=${encodeURIComponent(q)}` },
   { id: 'lidl', name: 'Lidl', logo: '/Lidl_Logo.svg', searchUrl: (q) => `https://www.lidl.co.uk/h/search?q=${encodeURIComponent(q)}` },
