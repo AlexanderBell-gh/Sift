ee200ac456a211d2eb20caf66d9f10a5cdbc0a52
Alex-Projects-Master <Inbox.AlexBell@outlook.com>
2026-08-26 16:47:44 +0100

    Added real browser svg's for ExtensionFAB.tsx with new browser detection

--- src/components/ExtensionFAB.tsx
+++ src/components/ExtensionFAB.tsx
@@ -1,25 +1,36 @@
 import { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useExtensionInstalled } from '../hooks/useExtensionInstalled';
+import { useBrowser } from '../hooks/useBrowser';
+import { ChromeIcon, FirefoxIcon, EdgeIcon, OtherIcon } from './ui/BrowserIcons';
 import { cn } from '../lib/utils';
 
-function FabIcon({ className }: { className?: string }) {
-  return (
-    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className}>
-      <path fill="#fff" d="M10.18 0c-2.081 0-3.807 1.608-4 3.64H4.019A4.033 4.033 0 0 0 0 7.66v4.017h1.498a2.13 2.13 0 0 1 2.143 2.144 2.13 2.13 0 0 1-2.143 2.143H0V24h8.036v-1.498a2.13 2.13 0 0 1 2.144-2.143 2.13 2.13 0 0 1 2.143 2.143V24h4.018a4.03 4.03 0 0 0 4.018-4.018v-2.163C22.392 17.627 24 15.901 24 13.821s-1.608-3.807-3.64-4V7.66a4.03 4.03 0 0 0-4.019-4.018h-2.162C13.986 1.608 12.26 0 10.179 0m0 1.875a2.13 2.13 0 0 1 2.143 2.143v1.498h4.018a2.13 2.13 0 0 1 2.143 2.143v4.018h1.498a2.13 2.13 0 0 1 2.143 2.144 2.13 2.13 0 0 1-2.143 2.143h-1.498v4.018a2.13 2.13 0 0 1-2.143 2.143h-2.162c-.193-2.033-1.919-3.64-4-3.64s-3.806 1.607-3.998 3.64H1.875V17.82c2.033-.192 3.64-1.918 3.64-3.998s-1.607-3.807-3.64-4V7.66a2.13 2.13 0 0 1 2.143-2.143h4.018V4.018a2.13 2.13 0 0 1 2.144-2.143" />
-    </svg>
-  );
-}
+const BROWSER_ICONS = {
+  chrome: ChromeIcon,
+  firefox: FirefoxIcon,
+  edge: EdgeIcon,
+  other: OtherIcon,
+} as const;
+
+const EXTENSION_URLS = {
+  chrome: 'https://github.com/Alex-Projects-Master/sift-extension/releases/download/v0.2.5/sift-extension-0.2.5-chrome.zip',
+  firefox: '#',
+  edge: '#',
+  other: '#',
+} as const;
 
 type FabState = 'hidden' | 'visible' | 'dismissing';
 
 export default function ExtensionFAB() {
   const location = useLocation();
   const { installed } = useExtensionInstalled();
+  const { browser } = useBrowser();
   const [fabState, setFabState] = useState<FabState>('hidden');
   const [expanded, setExpanded] = useState(false);
   const panelRef = useRef<HTMLDivElement>(null);
   const prevInstalled = useRef(installed);
+
+  const BrowserIcon = BROWSER_ICONS[browser];
 
   const isAuthPage = location.pathname === '/' || location.pathname === '/auth';
 
@@ -77,7 +88,7 @@ export default function ExtensionFAB() {
             </div>
           </div>
           <a
-            href="https://github.com/Alex-Projects-Master/sift-extension/releases/download/v0.2.5/sift-extension-0.2.5-chrome.zip"
+            href={EXTENSION_URLS[browser]}
             target="_blank"
             rel="noopener noreferrer"
             className="extension-fab-download"
@@ -91,7 +102,7 @@ export default function ExtensionFAB() {
         className="extension-fab-btn"
         aria-label="Get the Sift browser extension"
       >
-        <FabIcon className="extension-fab-icon" />
+        <BrowserIcon className="extension-fab-icon" />
       </button>
     </div>
   );
---
+++ src/components/ui/BrowserIcons.tsx
@@ -0,0 +1,117 @@
+interface IconProps {
+  className?: string;
+}
+
+export function ChromeIcon({ className }: IconProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 190.5 190.5" className={className}>
+      <path fill="#fff" d="M95.252 142.873c26.304 0 47.627-21.324 47.627-47.628s-21.323-47.628-47.627-47.628-47.627 21.324-47.627 47.628 21.323 47.628 47.627 47.628z" />
+      <path fill="#229342" d="m54.005 119.07-41.24-71.43a95.227 95.227 0 0 0-.003 95.25 95.234 95.234 0 0 0 82.496 47.61l41.24-71.43v-.011a47.613 47.613 0 0 1-17.428 17.443 47.62 47.62 0 0 1-47.632.007 47.62 47.62 0 0 1-17.433-17.437z" />
+      <path fill="#fbc116" d="m136.495 119.067-41.239 71.43a95.229 95.229 0 0 0 82.489-47.622A95.24 95.24 0 0 0 190.5 95.248a95.237 95.237 0 0 0-12.772-47.623H95.249l-.01.007a47.62 47.62 0 0 1 23.819 6.372 47.618 47.618 0 0 1 17.439 17.431 47.62 47.62 0 0 1-.001 47.633z" />
+      <path fill="#1a73e8" d="M95.252 132.961c20.824 0 37.705-16.881 37.705-37.706S116.076 57.55 95.252 57.55 57.547 74.431 57.547 95.255s16.881 37.706 37.705 37.706z" />
+      <path fill="#e33b2e" d="M95.252 47.628h82.479A95.237 95.237 0 0 0 142.87 12.76 95.23 95.23 0 0 0 95.245 0a95.222 95.222 0 0 0-47.623 12.767 95.23 95.23 0 0 0-34.856 34.872l41.24 71.43.011.006a47.62 47.62 0 0 1-.015-47.633 47.61 47.61 0 0 1 41.252-23.815z" />
+    </svg>
+  );
+}
+
+export function FirefoxIcon({ className }: IconProps) {
+  return (
+    <svg viewBox="0 0 256 265" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" className={className}>
+      <defs>
+        <radialGradient id="ff-b" cx="-7907.187" cy="-8515.121" r="80.797" gradientTransform="translate(26367.938 28186.305) scale(3.3067)" gradientUnits="userSpaceOnUse">
+          <stop offset=".129" stopColor="#ffbd4f" />
+          <stop offset=".186" stopColor="#ffac31" />
+          <stop offset=".247" stopColor="#ff9d17" />
+          <stop offset=".283" stopColor="#ff980e" />
+          <stop offset=".403" stopColor="#ff563b" />
+          <stop offset=".467" stopColor="#ff3750" />
+          <stop offset=".71" stopColor="#f5156c" />
+          <stop offset=".782" stopColor="#eb0878" />
+          <stop offset=".86" stopColor="#e50080" />
+        </radialGradient>
+        <radialGradient id="ff-c" cx="-7936.711" cy="-8482.089" r="80.797" gradientTransform="translate(26367.938 28186.305) scale(3.3067)" gradientUnits="userSpaceOnUse">
+          <stop offset=".3" stopColor="#960e18" />
+          <stop offset=".351" stopColor="#b11927" stopOpacity=".74" />
+          <stop offset=".435" stopColor="#db293d" stopOpacity=".343" />
+          <stop offset=".497" stopColor="#f5334b" stopOpacity=".094" />
+          <stop offset=".53" stopColor="#ff3750" stopOpacity="0" />
+        </radialGradient>
+        <radialGradient id="ff-d" cx="-7926.97" cy="-8533.457" r="58.534" gradientTransform="translate(26367.938 28186.305) scale(3.3067)" gradientUnits="userSpaceOnUse">
+          <stop offset=".132" stopColor="#fff44f" />
+          <stop offset=".252" stopColor="#ffdc3e" />
+          <stop offset=".506" stopColor="#ff9d12" />
+          <stop offset=".526" stopColor="#ff980e" />
+        </radialGradient>
+        <radialGradient id="ff-e" cx="-7945.648" cy="-8460.984" r="38.471" gradientTransform="translate(26367.938 28186.305) scale(3.3067)" gradientUnits="userSpaceOnUse">
+          <stop offset=".353" stopColor="#3a8ee6" />
+          <stop offset=".472" stopColor="#5c79f0" />
+          <stop offset=".669" stopColor="#9059ff" />
+          <stop offset="1" stopColor="#c139e6" />
+        </radialGradient>
+        <radialGradient id="ff-f" cx="-7935.62" cy="-8491.546" r="20.397" gradientTransform="matrix(3.21411 -.77707 .90934 3.76302 33365.914 25904.014)" gradientUnits="userSpaceOnUse">
+          <stop offset=".2" stopColor="#ffe226" />
+          <stop offset=".522" stopColor="#f7c948" />
+          <stop offset=".808" stopColor="#e25241" />
+          <stop offset=".963" stopColor="#d93025" />
+        </radialGradient>
+      </defs>
+      <path fill="url(#ff-b)" d="M231.975 111.625c-.038-42.063-34.164-76.14-76.227-76.175-25.344 0-47.544 12.404-61.046 31.37a80.295 80.295 0 0 1 28.694-28.325C143.063 28.56 171.48 18.8 200.29 25.66c13.577 3.244 25.81 10.553 35.327 21.072a76.08 76.08 0 0 1 11.774 17.474c7.87 16.427 9.878 35.15 5.624 52.329a75.864 75.864 0 0 1-21.04 33.84 76.07 76.07 0 0 1-34.48 18.29c-12.026 3.1-24.78 3.6-36.944 1.45a75.866 75.866 0 0 1-32.79-17.305 76.073 76.073 0 0 1-22.966-33.13 75.917 75.917 0 0 1-3.47-38.848 75.817 75.817 0 0 1 14.05-27.116 75.888 75.888 0 0 1 24.15-18.275 75.9 75.9 0 0 1 28.74-6.845c12.666-.218 25.1 2.73 36.2 8.528a75.87 75.87 0 0 1 25.133 19.873l.433.591-33.485 21.913-.42-.626a51.263 51.263 0 0 0-17.13-13.585 51.198 51.198 0 0 0-19.602-5.838 51.284 51.284 0 0 0-32.423 7.236 51.285 51.285 0 0 0-18.86 18.604 51.256 51.256 0 0 0-2.322 37.682 51.28 51.28 0 0 0 16.554 22.785 51.29 51.29 0 0 0 28.154 9.345c9.584-.045 18.86-3.5 26.283-9.81a51.17 51.17 0 0 0 16.377-24.883c2.89-12.04 2.598-24.756-.833-36.574l-.003-.012.046-.027z" />
+      <path fill="url(#ff-c)" d="M154.922 88.91a51.15 51.15 0 0 0-35.77-12.358 51.282 51.282 0 0 0-32.423 7.236 51.285 51.285 0 0 0-18.86 18.604 51.256 51.256 0 0 0-2.322 37.682 51.28 51.28 0 0 0 16.554 22.785 51.29 51.29 0 0 0 28.154 9.345c9.584-.045 18.86-3.5 26.283-9.81a51.17 51.17 0 0 0 16.377-24.883c2.89-12.04 2.598-24.756-.833-36.574l-.003-.012.046-.027c-.038-42.063-34.164-76.14-76.227-76.175-5.233 0-10.36.537-15.333 1.56a76.247 76.247 0 0 0-41.722 30.115A80.28 80.28 0 0 1 80.7 54.548C94.203 35.58 116.402 23.175 140.98 23.175c32.728 0 60.75 21.903 69.635 51.803a75.85 75.85 0 0 1 5.625 52.33 76.08 76.08 0 0 1-11.775 17.473l-.407.555-34.666-22.27.425-.586-.003-.012-.003-.012z" />
+      <path fill="url(#ff-d)" d="M121.318 217.538a51.286 51.286 0 0 1-28.154-9.345A51.28 51.28 0 0 1 76.61 185.41a51.256 51.256 0 0 1 2.322-37.682 51.285 51.285 0 0 1 18.86-18.605 51.198 51.198 0 0 1 19.602-5.837 51.263 51.263 0 0 1 17.13 3.025l.42.626 33.485-21.913-.433-.59a75.87 75.87 0 0 0-25.133-19.874 75.9 75.9 0 0 0-28.74-6.845c-9.776.007-19.237 2.845-27.4 8.245A80.28 80.28 0 0 0 50.754 132a80.295 80.295 0 0 0-28.695 28.325C35.561 143.29 17.36 130.88 0 130.88c0 32.728 21.904 60.75 51.803 69.635a75.864 75.864 0 0 0 22.68 6.177 75.95 75.95 0 0 0 46.835-9.154z" />
+    </svg>
+  );
+}
+
+export function EdgeIcon({ className }: IconProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className={className}>
+      <defs>
+        <radialGradient id="edge-b" cx="161.8" cy="68.9" r="95.4" gradientTransform="matrix(1 0 0 -.95 0 248.8)" gradientUnits="userSpaceOnUse">
+          <stop offset=".7" stopOpacity="0" />
+          <stop offset=".9" stopOpacity=".5" />
+          <stop offset="1" />
+        </radialGradient>
+        <radialGradient id="edge-d" cx="-340.3" cy="63" r="143.2" gradientTransform="matrix(.15 -.99 -.8 -.12 176.6 -125.4)" gradientUnits="userSpaceOnUse">
+          <stop offset=".8" stopOpacity="0" />
+          <stop offset=".9" stopOpacity=".5" />
+          <stop offset="1" />
+        </radialGradient>
+        <radialGradient id="edge-e" cx="113.4" cy="570.2" r="202.4" gradientTransform="matrix(-.04 1 2.13 .08 -1179.5 -106.7)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#35c1f1" />
+          <stop offset=".1" stopColor="#34c1ed" />
+          <stop offset=".2" stopColor="#2fc2df" />
+          <stop offset=".3" stopColor="#2bc3d2" />
+          <stop offset=".7" stopColor="#36c752" />
+        </radialGradient>
+        <radialGradient id="edge-f" cx="376.5" cy="568" r="97.3" gradientTransform="matrix(.28 .96 .78 -.23 -303.8 -148.5)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#66eb6e" />
+          <stop offset="1" stopColor="#66eb6e" stopOpacity="0" />
+        </radialGradient>
+        <linearGradient id="edge-a" x1="63.3" x2="241.7" y1="84" y2="84" gradientTransform="matrix(1 0 0 -1 0 266)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#0c59a4" />
+          <stop offset="1" stopColor="#114a8b" />
+        </linearGradient>
+        <linearGradient id="edge-c" x1="157.3" x2="46" y1="161.4" y2="40.1" gradientTransform="matrix(1 0 0 -1 0 266)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#1b9de2" />
+          <stop offset=".2" stopColor="#1595df" />
+          <stop offset=".7" stopColor="#0680d7" />
+          <stop offset="1" stopColor="#0078d4" />
+        </linearGradient>
+      </defs>
+      <path fill="url(#edge-a)" d="M235.7 195.5a93.7 93.7 0 0 1-10.6 4.7 101.9 101.9 0 0 1-35.9 6.4c-47.3 0-88.5-32.5-88.5-74.3a31.5 31.5 0 0 1 16.4-27.3c-42.8 1.8-53.8 46.4-53.8 72.5 0 74 68.1 81.4 82.8 81.4 7.9 0 19.8-2.3 27-4.6l1.3-.4a128.3 128.3 0 0 0 66.6-52.8 4 4 0 0 0-5.3-5.6Z" transform="translate(-4.6 -5)" />
+      <path fill="url(#edge-b)" d="M235.7 195.5a93.7 93.7 0 0 1-10.6 4.7 101.9 101.9 0 0 1-35.9 6.4c-47.3 0-88.5-32.5-88.5-74.3a31.5 31.5 0 0 1 16.4-27.3c-42.8 1.8-53.8 46.4-53.8 72.5 0 74 68.1 81.4 82.8 81.4 7.9 0 19.8-2.3 27-4.6l1.3-.4a128.3 128.3 0 0 0 66.6-52.8 4 4 0 0 0-5.3-5.6Z" opacity=".35" style={{ isolation: 'isolate' }} transform="translate(-4.6 -5)" />
+      <path fill="url(#edge-c)" d="M110.3 246.3A79.2 79.2 0 0 1 87.6 225a80.7 80.7 0 0 1 29.5-120c3.2-1.5 8.5-4.1 15.6-4a32.4 32.4 0 0 1 25.7 13 31.9 31.9 0 0 1 6.3 18.7c0-.2 24.5-79.6-80-79.6-43.9 0-80 41.6-80 78.2a130.2 130.2 0 0 0 12.1 56 128 128 0 0 0 156.4 67 75.5 75.5 0 0 1-62.8-8Z" transform="translate(-4.6 -5)" />
+      <path fill="url(#edge-d)" d="M110.3 246.3A79.2 79.2 0 0 1 87.6 225a80.7 80.7 0 0 1 29.5-120c3.2-1.5 8.5-4.1 15.6-4a32.4 32.4 0 0 1 25.7 13 31.9 31.9 0 0 1 6.3 18.7c0-.2 24.5-79.6-80-79.6-43.9 0-80 41.6-80 78.2a130.2 130.2 0 0 0 12.1 56 128 128 0 0 0 156.4 67 75.5 75.5 0 0 1-62.8-8Z" opacity=".41" style={{ isolation: 'isolate' }} transform="translate(-4.6 -5)" />
+      <path fill="url(#edge-e)" d="M157 153.8c-.9 1-3.4 2.5-3.4 5.6 0 2.6 1.7 5.2 4.8 7.3 14.3 10 41.4 8.6 41.5 8.6a59.6 59.6 0 0 0 30.3-8.3 61.4 61.4 0 0 0 30.4-52.9c.3-22.4-8-37.3-11.3-43.9C228 28.8 182.3 5 132.6 5a128 128 0 0 0-128 126.2c.5-36.5 36.8-66 80-66 3.5 0 23.5.3 42 10a72.6 72.6 0 0 1 30.9 29.3c6.1 10.6 7.2 24.1 7.2 29.5s-2.7 13.3-7.8 19.9Z" transform="translate(-4.6 -5)" />
+      <path fill="url(#edge-f)" d="M157 153.8c-.9 1-3.4 2.5-3.4 5.6 0 2.6 1.7 5.2 4.8 7.3 14.3 10 41.4 8.6 41.5 8.6a59.6 59.6 0 0 0 30.3-8.3 61.4 61.4 0 0 0 30.4-52.9c.3-22.4-8-37.3-11.3-43.9C228 28.8 182.3 5 132.6 5a128 128 0 0 0-128 126.2c.5-36.5 36.8-66 80-66 3.5 0 23.5.3 42 10a72.6 72.6 0 0 1 30.9 29.3c6.1 10.6 7.2 24.1 7.2 29.5s-2.7 13.3-7.8 19.9Z" transform="translate(-4.6 -5)" />
+    </svg>
+  );
+}
+
+export function OtherIcon({ className }: IconProps) {
+  return (
+    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className={className}>
+      <path fill="#fff" d="M10.18 0c-2.081 0-3.807 1.608-4 3.64H4.019A4.033 4.033 0 0 0 0 7.66v4.017h1.498a2.13 2.13 0 0 1 2.143 2.144 2.13 2.13 0 0 1-2.143 2.143H0V24h8.036v-1.498a2.13 2.13 0 0 1 2.144-2.143 2.13 2.13 0 0 1 2.143 2.143V24h4.018a4.03 4.03 0 0 0 4.018-4.018v-2.163C22.392 17.627 24 15.901 24 13.821s-1.608-3.807-3.64-4V7.66a4.03 4.03 0 0 0-4.019-4.018h-2.162C13.986 1.608 12.26 0 10.179 0m0 1.875a2.13 2.13 0 0 1 2.143 2.143v1.498h4.018a2.13 2.13 0 0 1 2.143 2.143v4.018h1.498a2.13 2.13 0 0 1 2.143 2.144 2.13 2.13 0 0 1-2.143 2.143h-1.498v4.018a2.13 2.13 0 0 1-2.143 2.143h-2.162c-.193-2.033-1.919-3.64-4-3.64s-3.806 1.607-3.998 3.64H1.875V17.82c2.033-.192 3.64-1.918 3.64-3.998s-1.607-3.807-3.64-4V7.66a2.13 2.13 0 0 1 2.143-2.143h4.018V4.018a2.13 2.13 0 0 1 2.144-2.143" />
+    </svg>
+  );
+}
